113941fb75ab44996d0914a423b6b8f3e3c3c1b0
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-05-25 16:15:07 -0300

    Configure jQuery.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'APP';
+
+    public ngOnInit() {
+        $(document).ready(function () {
+            $("button").click(function () {
+                var div = $("div");
+                div.animate({left: '100px'}, "slow");
+                div.animate({fontSize: '5em'}, "slow");
+            });
+        });
+    }
 }
