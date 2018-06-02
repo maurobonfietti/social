de7f8304071e505ab8d880af1456a3403910a462
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-06-02 16:32:31 -0300

    Added form for register new users.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,14 +1,21 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {User} from '../../models/user';
 
 @Component({
     selector: 'register',
     templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
-    public title:string;
+    public title: string;
+    public user: User;
 
-    constructor() {
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {
         this.title = 'Registrate';
+        this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
     }
 
     ngOnInit() {
