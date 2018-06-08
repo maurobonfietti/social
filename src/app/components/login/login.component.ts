20bbdc60a9c9af7a06886a58235850c6d87de388
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-06-07 23:48:39 -0300

    Layout login form and making login component.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,34 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
     selector: 'login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
     public title:string;
+    public user: User;
+    public status: string;
 
-    constructor() {
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userService: UserService
+    ) {
         this.title = 'Identificate';
+        this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
     }
 
     ngOnInit() {
         console.log('Componente login cargado.');
+    }
+
+    onSubmit() {
+        alert(this.user.email);
+        alert(this.user.password);
+        console.log(this.user);
     }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
 
     register(user: User): Observable<any> {
         let params = JSON.stringify(user);
-        let headers = new HttpHeaders().set('Content-Type', 'applicaction/json');
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
 
         return this._http.post(this.url+'register', params, {headers: headers});
     }
