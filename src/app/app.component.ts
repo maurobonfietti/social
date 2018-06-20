15b6e1535fd84632570df5d63b1a20eff6bd660b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-06-20 00:08:45 -0300

    Logout functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, DoCheck} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 import {UserService} from './services/user.service';
 
 @Component({
@@ -12,6 +13,8 @@ export class AppComponent implements OnInit, DoCheck {
     public identity;
 
     constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
         private _userService: UserService
     ) {
         this.title = 'SOCIAL';
@@ -24,5 +27,11 @@ export class AppComponent implements OnInit, DoCheck {
 
     ngDoCheck() {
         this.identity = this._userService.getIdentity();
+    }
+
+    logout() {
+        localStorage.clear();
+        this.identity = null;
+        this._router.navigate(['/']);
     }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -59,6 +59,7 @@ export class LoginComponent implements OnInit {
                 } else {
                     this.status = 'success';
                     localStorage.setItem('token', JSON.stringify(this.token));
+                    this._router.navigate(['/']);
                 }
             },
             error => {
