c73ac61efaacb66329aa06ee37c65b38ab55bc60
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-09-29 00:19:46 -0300

    Adding buttons to follow people.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -20,6 +20,7 @@ export class UsersComponent implements OnInit {
     public total;
     public pages;
     public users: User[];
+    public follows;
     public status: string;
 
     constructor(
@@ -73,7 +74,10 @@ export class UsersComponent implements OnInit {
                     this.total = response.total;
                     this.users = response.users;
                     this.pages = response.pages;
-                    console.log(page);
+                    this.follows = response.user_following;
+//                    console.log(response);
+//                    console.log(this.follows);
+//                    console.log(page);
                     if (page > this.pages) {
                         this._router.navigate(['/gente', 1]);
                     }
@@ -88,5 +92,14 @@ export class UsersComponent implements OnInit {
                 }
             }
         );
+    }
+
+    public followUserOver;
+
+    mouseEnter(user_id) {
+        this.followUserOver = user_id;
+    }
+    mouseLeave(user_id) {
+        this.followUserOver = 0;
     }
 }
