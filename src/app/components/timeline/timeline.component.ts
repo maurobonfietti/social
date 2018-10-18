7f86696a233f6232b5ff53df43f043acd31c4cf1
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-10-18 10:45:44 -0300

    Show publications timeline.

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -3,30 +3,65 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 import {Publication} from '../../models/publication';
 import {GLOBAL} from '../../services/global';
 import {UserService} from '../../services/user.service';
+import {PublicationService} from '../../services/publication.service';
 
 @Component({
     selector: 'timeline',
     templateUrl: './timeline.component.html',
-    providers: [UserService]
+    providers: [UserService, PublicationService]
 })
 export class TimelineComponent implements OnInit {
     public identity;
     public token;
     public title: string;
     public url: string;
+    public status: string;
+    public page: string;
+    public total;
+    public pages;
+    public publications: Publication[];
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
-        private _userService: UserService
+        private _userService: UserService,
+        private _publicationService: PublicationService
     ) {
         this.title = 'Timeline';
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
         this.url = GLOBAL.url;
+        this.page = 1;
     }
 
     ngOnInit() {
         console.log('Componente timeline cargado.');
+        this.getPublications(this.page);
+    }
+
+    getPublications(page) {
+        this._publicationService.getPublication(this.token, page).subscribe(
+            response => {
+                console.log(response);
+                if (response.publication) {
+                    this.total = response.total_items;
+                    this.pages = response.pages;
+                    this.publications = response.publication;
+                    if (page > this.pages) {
+                        this._router.navigate(['/home']);
+                    }
+                } else {
+                    this.status = 'error';
+                }
+            },
+            error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if (errorMessage != null) {
+                    this.status = 'error';
+                }
+            }
+        );
     }
 }
