d0765d1883dc6faa3850c53ed7bde88e28f9b957
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-10-20 00:41:07 -0300

    Animated infinite scroll :-)

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -54,6 +54,7 @@ export class TimelineComponent implements OnInit {
                         var arrayA = this.publications;
                         var arrayB = response.publications;
                         this.publications = arrayA.concat(arrayB);
+                        $("html, body").animate({scrollTop: $('body').prop("scrollHeight")}, 500);
                     }
                     if (page > this.pages) {
                         this._router.navigate(['/home']);
