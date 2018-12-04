ea66df533926d1c87818462771124d22d362dfa2
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-12-04 11:37:45 -0300

    Show buttons in home page, only if user is logged.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../services/user.service';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    providers: [UserService]
 })
 export class HomeComponent implements OnInit {
     public title: string;
+    public identity;
 
-    constructor() {
+    constructor(
+        private _userService: UserService
+    ) {
         this.title = 'Bienvenido!';
     }
 
     ngOnInit() {
         console.log('Componente home cargado.');
         console.log('app version 0.0.1');
+        this.identity = this._userService.getIdentity();
+//        console.log(this.identity);
     }
 
     onSubmit() {
