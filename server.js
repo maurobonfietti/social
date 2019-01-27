3f86daa3fa3da0caade486720585d15c710863c1
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-01-27 20:07:52 -0300

    Preparing to deploy with Heroku.

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static(__dirname + '/dist'));
+//const express = require('express');
+//const app = express();
+//const path = require('path');
+//app.use(express.static(__dirname + '/dist'));
+//app.listen(process.env.PORT || 8080);
+//app.get('/*', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/dist/index.html'));
+//});
+var express = require("express");
+//var bodyParser = require("body-parser");
+//var mongodb = require("mongodb");
+//var ObjectID = mongodb.ObjectID;
+
+//var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+//app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,3 +1,4 @@
 export var GLOBAL = {
-    url: 'http://localhost:3800/api/'
+//    url: 'http://localhost:3800/api/'
+    url: 'http://social-app-back.herokuapp.com/'
 }
