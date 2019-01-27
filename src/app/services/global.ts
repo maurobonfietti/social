917791e17e1f1c6f6faec1876db94bf192c2d478
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-01-27 20:45:22 -0300

    Added apiUrl as environment variable.

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,7 @@
+import {environment} from '../environments/environment';
+
 export var GLOBAL = {
 //    url: 'http://localhost:3800/api/'
-    url: 'https://social-app-back.herokuapp.com/api/'
+//    url: 'https://social-app-back.herokuapp.com/api/'
+    url: environment.apiUrl
 }
